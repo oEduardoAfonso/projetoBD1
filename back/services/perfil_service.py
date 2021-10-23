d475780b6c7b3f8c3a148701831a08331c6aa48f
Eduardo Afonso <oeduardoafonso@gmail.com>
2021-10-23 15:17:22 -0300

    Adicionando metodo post de perfil

--- back/services/perfil_service.py
+++ back/services/perfil_service.py
@@ -1,6 +1,25 @@
+from io import BytesIO
+import os
+from flask.helpers import send_file
 from ..models import models
-from back import db
+from back import db, app
+from flask import send_from_directory
 
 def listar_perfis():
     perfis = db.session.query(models.Perfil).all()
     return perfis
+
+def cadastrar_perfil(perfil):
+    try:
+        img = open('/home/edu/faculdade/5sem/bancos/projetoBD1/back/static/foto/foto.png', 'rb').read()
+        perfil_novo = models.Perfil(
+            cpf=perfil['cpf'],
+            nome=perfil['nome'],
+            foto=img
+            )
+    except FileNotFoundError:
+        return(404)
+    db.session.add(perfil_novo)
+    db.session.commit()
+    breakpoint()
+    return perfil_novo
--- back/views/perfil_view.py
+++ back/views/perfil_view.py
@@ -4,12 +4,22 @@ from back import api, db, ma
 from flask import request, jsonify
 from ..schemas import perfil_schema
 from ..services import perfil_service
+from ..models import models
 
 class PerfilList(Resource):
     def get(self):
-        
-        ps = perfil_schema.PerfilSchema(many=True)
+        ps = perfil_schema.PerfilSchemaOut(many=True)
         resultados = perfil_service.listar_perfis()
         return make_response(ps.jsonify(resultados), 200)
+
+    def post(self):
+        ps = perfil_schema.PerfilSchemaIn()
+        perfil = ps.load(request.json)
+        resultado = perfil_service.cadastrar_perfil(perfil)
+        
+        if(resultado != 404):
+            return make_response(ps.jsonify(resultado), 200)
+
+        return make_response(jsonify('Imagem nao encontrada'), 404)
 
 api.add_resource(PerfilList, '/perfis')
