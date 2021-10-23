51b1ed53f370cbf2d09f509eb9908ae09d655aa0
Eduardo Afonso <oeduardoafonso@gmail.com>
2021-10-23 15:16:50 -0300

    Adicionando schema de entrada e de saida

--- back/schemas/perfil_schema.py
+++ back/schemas/perfil_schema.py
@@ -1,10 +1,19 @@
 from back import ma
 from ..models import models
+from marshmallow import fields,validate
 
-class PerfilSchema(ma.SQLAlchemyAutoSchema):
+class PerfilSchemaOut(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = models.Perfil
         include_fk = True
         include_relationships = True
         load_instance = True
         exclude=('foto',)
+
+class PerfilSchemaIn(ma.SQLAlchemySchema):
+    class Meta:
+        model = models.Perfil
+
+    cpf = fields.String(required=True, validate=validate.Length(min=1, max=11))
+    nome = fields.String(required=True, validate=validate.Length(min=1, max=100))
+    
