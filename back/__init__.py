bd32ece5fd46459cc923aca8a5ec8c9537fad310
Eduardo Afonso <oeduardoafonso@gmail.com>
2021-10-23 13:27:27 -0300

    Configurando init

---
+++ back/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+
+app.config.from_object('config')
+
+api = Api(app)
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+from .views import perfil_view
