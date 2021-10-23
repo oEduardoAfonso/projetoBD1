bfb83106890db49563d5a60029eb4ce8c060e5bd
Eduardo Afonso <oeduardoafonso@gmail.com>
2021-10-23 15:41:03 -0300

    Finalizando CRUD de perfil

--- back/services/perfil_service.py
+++ back/services/perfil_service.py
@@ -1,9 +1,5 @@
-from io import BytesIO
-import os
-from flask.helpers import send_file
 from ..models import models
-from back import db, app
-from flask import send_from_directory
+from back import db
 
 def listar_perfis():
     perfis = db.session.query(models.Perfil).all()
@@ -21,5 +17,25 @@ def cadastrar_perfil(perfil):
         return(404)
     db.session.add(perfil_novo)
     db.session.commit()
-    breakpoint()
     return perfil_novo
+
+def listar_perfil_cpf(cpf):
+    perfil = db.session.query(models.Perfil).filter_by(cpf=cpf).first()
+    return perfil
+
+def editar_perfil(perfil_bd, perfil):
+    img = open('/home/edu/faculdade/5sem/bancos/projetoBD1/back/static/foto/foto.png', 'rb').read()
+    perfil_new = models.Perfil(
+        nome=perfil['nome'],
+        foto=img
+        )
+
+    if perfil_new.nome is not None:
+        perfil_bd.nome = perfil_new.nome
+    if perfil_new.foto is not None:
+        perfil_bd.foto = perfil_new.foto
+    db.session.commit()
+
+def deletar_perfil(perfil):
+    db.session.delete(perfil)
+    db.session.commit()
--- back/views/perfil_view.py
+++ back/views/perfil_view.py
@@ -1,10 +1,9 @@
 from flask.helpers import make_response
 from flask_restful import Resource
-from back import api, db, ma
+from back import api
 from flask import request, jsonify
 from ..schemas import perfil_schema
 from ..services import perfil_service
-from ..models import models
 
 class PerfilList(Resource):
     def get(self):
@@ -19,7 +18,38 @@ class PerfilList(Resource):
         
         if(resultado != 404):
             return make_response(ps.jsonify(resultado), 200)
-
         return make_response(jsonify('Imagem nao encontrada'), 404)
+    
+class SalaDetail(Resource):
+    def get(self, cpf):
+        perfil = perfil_service.listar_perfil_cpf(cpf)
 
+        if perfil is None:
+            return make_response(jsonify('Perfil nao encontrado'), 404)
+
+        ps = perfil_schema.PerfilSchemaOut()
+        return make_response(ps.jsonify(perfil), 200)
+
+    def put(self, cpf):
+        perfil_bd = perfil_service.listar_perfil_cpf(cpf)
+        if perfil_bd is None:
+            return make_response(jsonify('Perfil nao encontrada'), 404)
+
+        psIn = perfil_schema.PerfilSchemaIn()
+        perfil = psIn.load(request.json)
+        perfil_service.editar_perfil(perfil_bd, perfil)
+
+        psOut = perfil_schema.PerfilSchemaOut()
+        perfil_editado = perfil_service.listar_perfil_cpf(cpf)
+        return make_response(psOut.jsonify(perfil_editado), 200)
+
+    def delete(self, cpf):
+        perfil = perfil_service.listar_perfil_cpf(cpf)
+        if perfil is None:
+            return make_response(jsonify('Perfil nao encontrada'), 404)
+
+        perfil_service.deletar_perfil(perfil)
+        return make_response('', 204)
+        
 api.add_resource(PerfilList, '/perfis')
+api.add_resource(SalaDetail, '/perfis/<string:cpf>')
