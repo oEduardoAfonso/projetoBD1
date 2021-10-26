1ca37787cee4fabd8ec421f45b09d2de1757b508
Eduardo Afonso <oeduardoafonso@gmail.com>
2021-10-26 20:40:48 -0300

    Criando conexao feed

--- front/src/views/Feed.js
+++ front/src/views/Feed.js
@@ -5,28 +5,80 @@ import IconButton from '@mui/material/IconButton';
 import SendIcon from '@mui/icons-material/Send';
 import { TextField } from '@mui/material';
 import Box from '@mui/material/Box';
+import api from "../services/Api";
 
 
 export default function Feed() {
+    const [perfil, setPerfil] = React.useState(null);
+    const [conteudo, setConteudo] = React.useState('');
+    const [publicacoes, setPublicacoes] = React.useState([]);
+    const [update, setUpdate] = React.useState(false);
 
-    function postagens() {
+    React.useEffect(() => {
+        api.get('/perfis/' + localStorage.getItem('perfil'), {}).then(response => {
+            setPerfil(response.data)
+            setPublicacoes([])
+            response.data.amigos_perfil1_fkey_perfil.push(response.data.usuario)
+            response.data.amigos_perfil1_fkey_perfil.map(amigo => {
+                api.get('publicacoes/perfil/' + amigo, {}).then(response => {
+                    response.data.map(publicacao => {
+                        if (publicacao.codigo_comunidade == null) {
+                            async function getNome() {
+                                const resposta = api.get('/perfis/' + amigo, {}).then(response => response.data.nome)
+                                publicacao.nome = await resposta
+                                setPublicacoes(publicacoes => [...publicacoes, publicacao])
+                            }
+                            getNome()
+                        }
+                    })
+                })
+            })
+        })
+    }, [update])
+
+    function showPublicacoes() {
         var posts = []
-        for (var aux = 0; aux < 10; aux++)
-            posts.push(<Publicacao />)
-        return posts
+
+        posts = publicacoes.sort((a, b) => {
+            return a.data_hora < b.data_hora ? 1 : -1
+        })
+
+        const publi = posts.map(post => {
+            return <Publicacao
+                nome={post.nome}
+                data={post.data_hora}
+                conteudo={post.conteudo}
+                curtidas={post.curtida_codigo_publicacao_fkey_perfil.length}
+            />
+        })
+
+        return publi
+    }
+
+    const handleChange = event => {
+        setConteudo(event.target.value)
     }
 
     const publicar = () => {
-        console.log('publicado')
+        const post = {
+            conteudo: conteudo,
+            codigo_comunidade: null,
+            autor: perfil.usuario
+        }
+        api.post('/publicacoes', post).then(() => {
+            api.get('/perfis/' + perfil.usuario, {}).then(response => setPerfil(response.data))
+            setConteudo('')
+            setUpdate(!update)
+        })
     }
 
     return <Base>
-        <TextField multiline id="publicacao" placeholder="Sobre o que você está pensando?" variant="outlined" sx={{ width: '100%', p: 2 }} />
-        <Box sx={{display: 'flex'}}>
+        <TextField multiline id="publicacao" value={conteudo} onChange={handleChange} placeholder="Sobre o que você está pensando?" variant="outlined" sx={{ width: '100%', p: 2 }} />
+        <Box sx={{ display: 'flex' }}>
             <IconButton color="primary" aria-label="upload picture" component="span" onClick={publicar} sx={{ ml: 'auto', mr: 2 }}>
                 <SendIcon />
             </IconButton>
         </Box>
-        {postagens()}
+        {showPublicacoes()}
     </Base>
 }
